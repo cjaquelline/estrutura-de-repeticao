43503960bd56109c6a0b8f6a0b91158a0d475f6d
cjaquelliness@gmail.com <cjaquelliness@gmail.com>
2025-04-08 14:57:22 -0300

    Inicio

---
+++ repeticao.js
@@ -0,0 +1,45 @@
+
+function basico1() {
+    for (let n = 1; n <= 10; n++) {
+        console.log(" Contagem " + n);
+
+
+
+    }
+}
+
+
+function basico2() {
+
+   
+    let numero = prompt("Digite um numero:");
+
+    for (i = 1; i <= 10; i++) {
+        // console.log(`${numero} x ${i} = ${numero * 1}`);
+
+
+    }
+}
+
+function basico3 () {
+
+
+
+
+
+}
+
+
+function intermediario3 () {
+
+    debugger;
+
+    let n = 100;
+    while (n >= 1) {
+        console.log(n);
+        n++;
+    }
+
+
+} 
+intermediario3 ()
