36d23aee59ed6d616c81bdc67f9b4eff25fde0b2
Jaquelline <cjaquelliness@gmail.com>
2025-04-09 21:14:14 -0300

    Finalização

--- repeticao.js
+++ repeticao.js
@@ -39,34 +39,53 @@ function basico3() {
     alert(soma);
 }
 
-basico3()
 
 function intermediario3() {
 
+    let n = prompt("Digite um numero:")
+    while (n >= 0) {
+        console.log(n);
+        n--
+
+    }
+
+
+}
+
+
+function intermediario1() {
+
+    let valor = prompt("Digite o valor:");
+    for (let contador = 2; contador <= 50; contador += 2) {
+        alert(contador);
+    }
+
+}
+
+function intermediario2() {
+
     debugger;
 
-    let n = prompt("Digite um numero:")
+    let numeroAleatorio = Math.floor(Math.random() * (100 - 1) + 1);
 
-    while (let n = 100; n <= 0; n++ ) {
-        alert(n);
+    let tentativa;
 
+    while (tentativa != numeroAleatorio) {
+
+        tentativa = prompt("Innsira um numero:");
+
+        if (numeroAleatorio > tentativa) {
+            alert("Numero aleatorio é maior.");
+
+        } else if (numeroAleatorio < tentativa) {
+            alert("Numero é menor.");
+        } else {
+            alert ("Você ACERTOU!");
+
+        }
     }
 
-
 }
-
-function intermediario1 () {
-
-    let valor = prompt ("Digite o valor");
-    for (let contador= 2; contador <=50; contador +=2) {
-        alert (contador);
-    }
-
-
-
-}
-
-
-
+intermediario2()
 
 
